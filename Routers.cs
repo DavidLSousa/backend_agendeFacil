49670c53ebacf460c9d5b01c16ebab797a9ec585
David Sousa <davidlsousa.dev@gmail.com>
2025-04-08 13:34:49 -0300

    fix: add async/await

--- Routers.cs
+++ Routers.cs
@@ -10,30 +10,35 @@ namespace backend_agendeFacil
         {
             // Auth
             var authGroup = app.MapGroup("/api/auth");
-            authGroup.MapGet("/login", () => "Login!");
+            authGroup.MapPost("/login", () => "Login!");
 
             // Tenant
             var tenantGroup = app.MapGroup("/api/{id:guid}");
                 // Esse id virá do token
                 // Acessiveis so quando autenticadas autenticados pelo token JWT
-            tenantGroup.MapGet("/schedule", (Guid id, ScheduleController controller) => {
-                return controller.GetSchedule(id);
-            });
-            tenantGroup.MapGet("/solicitations", (Guid id, ScheduleController controller) => {
-                return controller.GetSolicitations(id);
-            });
-            tenantGroup.MapPut("/solicitations", (Guid id, Guid scheduleId, string status, ScheduleController controller) => {
-                return controller.UpdateSolicitation(id, scheduleId, status);
-            });
+            tenantGroup.MapGet("/schedule", 
+                async (Guid id, ScheduleController controller) => {
+                    return await controller.GetSchedule(id);
+                });
+            tenantGroup.MapGet("/solicitations", 
+                async (Guid id, ScheduleController controller) => {
+                    return await controller.GetSolicitations(id);
+                });
+            tenantGroup.MapPut("/solicitations", 
+                async (Guid id, Guid scheduleId, string status, ScheduleController controller) => {
+                    return await controller.UpdateSolicitation(id, scheduleId, status);
+                });
 
             // User
             var userGroup = app.MapGroup("/api/user");
-            userGroup.MapGet("/{id:guid}", async (Guid id, UserController controller) => {
-                return await controller.GetInfoTenantAsync(id);
-            });
-            userGroup.MapPost("/{id:guid}", (Guid id, HttpRequest request, UserController controller) => {
-                return controller.CreateSolicitation(id, request);
-            });    
+            userGroup.MapGet("/{id:guid}", 
+                async (Guid id, UserController controller) => {
+                    return await controller.GetInfoTenantAsync(id);
+                });
+            userGroup.MapPost("/{id:guid}", 
+                async (Guid id, HttpRequest request, UserController controller) => {
+                    return await controller.CreateSolicitation(id, request);
+                });    
         }
     }
 }
