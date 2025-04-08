c21801569d080b44ff6f1568620a9b8a7961a6cd
David Sousa <davidlsousa.dev@gmail.com>
2025-04-07 22:53:32 -0300

    add swagger

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
+// Enable Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// Routers
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
