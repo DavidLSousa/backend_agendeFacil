b0ddee256ba39a7dccdbbbfbc33a3d39dea255e8
David Sousa <davidlsousa.dev@gmail.com>
2025-04-08 00:45:55 -0300

    add endpoints

--- Routers.cs
+++ Routers.cs
@@ -5,8 +5,21 @@ namespace backend_agendeFacil
     {
         public static void Map(WebApplication app)
         {
-            app.MapGet("/", () => "Hello World!");
-            app.MapGet("/api", () => "Api");
+            // Auth
+            var authGroup = app.MapGroup("/api/auth");
+            authGroup.MapGet("/login", () => "Login!");
+
+            // Tenant
+            var tenantGroup = app.MapGroup("/api/{id}");
+            tenantGroup.MapGet("/schedule", (string id) => $"Tenant acessa sua agenda, {id}");
+            tenantGroup.MapGet("/solicitations", (string id) => "Lista de solicitações de agendamento");
+
+            // User
+            var userGroup = app.MapGroup("/api/user");
+            userGroup.MapGet("/{id}", (string id) => "Usuário seleciona tenant");
+                // Esse ID é o profissional e esse get retorna infos do profisisonla selecionado;
+            userGroup.MapGet("/{id}/solicitation", (string id) => "envio de form para agendamento");    
+                // Envio do form para agendamento, o qual deve ser enviado para o tenant;
         }
     }
 }
